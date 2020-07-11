011faffaa7d6463e975b06793323fc0b5c3acc9f
unknown <dj.zaragoza@icloud.com>
2020-07-11 12:19:54 -0700

    working on app functionality

--- app.js
+++ app.js
@@ -44,12 +44,71 @@ class UI {
 
       list.appendChild(row);
    }
+
+   static deleteBook(el) {
+      if (el.classList.contains('delete')) {
+         el.parentElement.parentElement.remove();
+      }
+   }
+
+   static showAlert(message, className) {
+      const div = document.createElement('div');
+      div.className = `alert-${className}`;
+      div.appendChild(document.createTextNode(message));
+      const container = document.querySelector('.container');
+      const form = document.querySelector('#book-form');
+      container.insertBefore(div, form);
+
+
+      // vanish in 5 seconds
+      setTimeout(() => document.querySelector('.alert').remove(), 5000);
+   }
+
+   static clearFields() {
+      document.querySelector('#title').value = '';
+      document.querySelector('#author').value = '';
+      document.querySelector('#isbn').value = '';
+   }
 }
 
+
 // Store class: handles local storage
+
+
 // Event: display books in the list
 
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 // Event: add a Book
-// Event: delete a Book
 
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+
+   // prevent actual submit
+   e.preventDefault();
+
+   // get form values
+   const title = document.querySelector('#title').value;
+   const author = document.querySelector('#author').value;
+   const isbn = document.querySelector('#isbn').value;
+
+   // validate
+   if (title === '' || author === '' || isbn === '') {
+      UI.showAlert('Please fill in all fields', 'danger');
+   } else {
+      // instantiate book
+      const book = new Book(title, author, isbn)
+
+      // add book to UI
+      UI.addBookToList(book);
+
+      // clear fields
+      UI.clearFields();
+
+      // console.log(book)
+   }
+});
+
+// Event: delete a Book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+   //console.log(e.target)
+   UI.deleteBook(e.target)
+})
